a8927628fe9230b104c0ed9187e78ddccf44eca9
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-06 11:12:02 -0300

    back correctamente configurado

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,16 @@
 import express from 'express'
-import http from 'http'
 import cors from 'cors'
-import  { Server }  from 'soket.io'
+import { Server } from "socket.io";
+import { createServer } from 'node:http'
 
 const app = express();
 app.use(cors());
 
-// app.get('/', (req, res) => {
-//   res.send("Server chat encendido!")
-// })
-
-const server = http.createServer(app)
-
+const server = createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST']
+    origin: "http://localhost:5173",
+    methods: ["GET","POST"]
   }
 })
 
