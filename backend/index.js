01c85fab52769904c9269b5f23afbd2f9b221e43
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-06 10:48:09 -0300

    server configurado

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,35 @@
-import express from 'express';
+import express from 'express'
 import http from 'http'
 import cors from 'cors'
+import  { Server }  from 'soket.io'
 
 const app = express();
 app.use(cors());
 
+// app.get('/', (req, res) => {
+//   res.send("Server chat encendido!")
+// })
+
 const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log(socket.id)
+  socket.on('disconnect', () => {
+    console.log('User disconnect', socket.id)
+  })
+});
 
 const PORT = 3001;
 
 server.listen(PORT, () => {
   console.log(`Server on in port: ${PORT}`)
 })
+
+
