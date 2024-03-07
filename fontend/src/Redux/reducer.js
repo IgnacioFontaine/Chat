b9919c3d254872d25586ac90c9999e0471a71a31
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-07 11:10:49 -0300

    reducer en proceso

---
+++ fontend/src/Redux/reducer.js
@@ -0,0 +1,28 @@
+import ACTION_TYPES from "./actionsTypes";
+
+//Config initialState
+const initialState = {
+  user:null,
+  error: false,
+  errormsg: {},
+};
+
+//Config reducer
+const reducer = (state = initialState, action) => {
+  console.log(action);
+  switch (action.type) {
+    
+    case ACTION_TYPES.ERROR:
+      return {
+        ...state,
+        error: true,
+      };
+
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+export default reducer;
