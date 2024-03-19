523309e0ce9e08518c3b76470317bb55754ced5d
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-18 21:34:26 -0300

    rooms component en proceso

--- fontend/src/Components/rooms.jsx
+++ fontend/src/Components/rooms.jsx
@@ -1,105 +1,101 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
-import Fab from '@mui/material/Fab';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
 import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import { Divider } from '@mui/material';
 
-const messages = [
+
+const rooms = [
   {
     id: 1,
     name: 'Trabajo',
-    secondary: "I'll be in the neighbourhood this week. Let's grab a bite to eat",
     person: 'T',
   },
   {
     id: 2,
     name: 'Familia',
-    secondary: `Do you have a suggestion for a good present for John on his work
-      anniversary. I am really confused & would love your thoughts on it.`,
     person: 'F',
   },
   {
     id: 3,
     name: 'Amigos',
-    secondary: 'I am try out this new BBQ recipe, I think this might be amazing',
     person: 'A',
   },
   {
     id: 4,
     name: 'Notas',
-    secondary: 'I have the tickets to the ReactConf for this year.',
     person: 'N',
   },
   {
     id: 5,
     name: 'Hermanos',
-    secondary: 'My appointment for the doctor was rescheduled for next Saturday.',
     person: "H",
   },
 ];
 
-const StyledFab = styled(Fab)({
-  position: 'absolute',
-  zIndex: 1,
-  top: -30,
-  left: 0,
-  right: 0,
-  margin: '0 auto',
-});
+
+function NewRoom() {
+  return (
+     <Box sx={{width:300}}>
+        <TextField
+          id="name"
+          label="Name"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          id="id"
+          label="ID"
+          variant="outlined"
+          fullWidth
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          backgroundColor="#6E2B98"
+          startIcon={<AddIcon  />}
+        >
+          Add
+        </Button>
+    </Box>
+  )
+}
+
 
 export default function BottomAppBar() {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Paper square sx={{ pb: '25px' }}>
-        <Typography variant="h5" gutterBottom component="div" sx={{ p: 2, pb: 0 }}>
+    <Box>
+      <Paper square sx={{ pb: '25px',width:400, borderRadius:2, backgroundColor:"#B94BFF", color:"black" }}>
+        <Typography variant="h5" gutterBottom component="div" sx={{ p: 2, pb: 0, fontFamily:"fantasy" }}>
           Salas
         </Typography>
-        <List sx={{ mb: 2 }}>
-          {messages.map(({ id, name, secondary, person }) => (
+        <Divider />
+        <List sx={{ mb: 1 }}>
+          {rooms.map(({ id, name, person }) => (
             <React.Fragment key={id}>
-              {id === 1 && (
-                <ListSubheader sx={{ bgcolor: 'background.paper' }}>
-                  Today
-                </ListSubheader>
-              )}
-
-              {id === 3 && (
-                <ListSubheader sx={{ bgcolor: 'background.paper' }}>
-                  Yesterday
-                </ListSubheader>
-              )}
 
               <ListItemButton>
                 <ListItemAvatar>
                   <Avatar alt="Profile Picture">{person}</Avatar>
                 </ListItemAvatar>
-                <ListItemText primary={name} secondary={secondary} />
+                <ListItemText primary={name}  />
               </ListItemButton>
             </React.Fragment>
           ))}
         </List>
+        <Divider />
+        <Box sx={{ml:5, mt:1}}>
+            <NewRoom />
+        </Box>
       </Paper>
-      <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
-        <Toolbar>
-          <StyledFab color="secondary" aria-label="add">
-            <AddIcon />
-          </StyledFab>
-          <Box sx={{ flexGrow: 1 }} />
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
+    </Box>
   );
 }
