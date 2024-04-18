eb4ff8b384180bbe15eaa5dddd11494696b71def
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-04-17 21:04:40 -0300

    nueva función de avatar room

--- fontend/src/Components/avatarRoom.jsx
+++ fontend/src/Components/avatarRoom.jsx
@@ -3,11 +3,18 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import { Avatar, Box, Button } from '@mui/material';
 import ClearIcon from '@mui/icons-material/Clear';
-
+import { deleteFirestoreRoom, selectRoom } from '../Redux/actions';
+import { useDispatch } from 'react-redux';
 
 const color_secondary = "#3E2A61";
 
 export const AvatarRoom = ({ name, id }) => {
+  const dispatch = useDispatch()
+  const handleDelete = () => {
+    dispatch(deleteFirestoreRoom(id))
+    dispatch(selectRoom(null))
+
+  }
   return (
     <Box key={id}>
       <ListItemButton sx={{
