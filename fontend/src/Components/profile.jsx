e4bd735241961b6040cb961618a0095e5e51900a
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-04-18 21:01:17 -0300

    nuevo profile pic

---
+++ fontend/src/Components/profile.jsx
@@ -0,0 +1,14 @@
+import { Box, Typography} from "@mui/material"
+
+
+function Profile() {
+  
+
+  return (
+    <Box>
+      <Typography>Name</Typography>
+    </Box>
+  );
+}
+
+export default Profile;
