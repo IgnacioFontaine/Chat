bbc18ea31ee1999d3c6086ffc5bb1be9cbd04fdc
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-04-23 12:35:48 -0300

    audio repro en proceso

--- fontend/src/Components/audioRepro.jsx
+++ fontend/src/Components/audioRepro.jsx
@@ -1,6 +1,7 @@
 import sound from "../assets/chat.mp3"
 import { Box } from "@mui/material"
-
+import { getUrlAudio } from "../Redux/actions";
+import { useDispatch } from "react-redux";
 
 function AudioRepro () {
   const message = {
