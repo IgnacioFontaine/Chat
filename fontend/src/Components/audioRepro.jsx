500e6f17c7d332668afc07ec6acb22e198c15179
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-04-24 14:04:54 -0300

    cambio en howl

--- fontend/src/Components/audioRepro.jsx
+++ fontend/src/Components/audioRepro.jsx
@@ -5,18 +5,19 @@ import { Box } from "@mui/material"
 import { Howl } from 'howler';
 
 
-function AudioRepro({message}) {
+function AudioRepro() {
   const sound = new Howl({
   src: ['https://firebasestorage.googleapis.com/v0/b/notwhatsapp-b1189.appspot.com/o/audio%2Fde31f679-380a-4413-8041-6cfc09030058?alt=media&token=5bfe849d-01d9-476d-a3e2-c8c1077ea39f'],
   html5: true, 
   preload: true,
-  autoplay: false 
+  autoplay: false,
+  format: ['mp3']
 });
   
   return (
     <Box>
       <audio controls >
-        <source src={sound} type="audio/mpeg" />
+        <source src={sound._src} type="audio/mpeg"/>
       </audio>
     </Box>
   );
