db4aa32cc962f46163b8574fd761c62fdb60e9fd
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-04-24 18:21:17 -0300

    cambio en audio repro

--- fontend/src/Components/audioRepro.jsx
+++ fontend/src/Components/audioRepro.jsx
@@ -1,13 +1,9 @@
 // import sound_dowload from "../assets/chat.mp3"
 import { Box } from "@mui/material"
 import { Howl } from 'howler';
-import { getUrlAudio } from "../Redux/actions";
-import { useDispatch } from "react-redux";
 
 
 function AudioRepro({ message }) {
-  // const dispatch = useDispatch()
-  // const url_dowload = dispatch(getUrlAudio(message))
   const sound = new Howl({
   src: [`${message.message}`],
   html5: true,
